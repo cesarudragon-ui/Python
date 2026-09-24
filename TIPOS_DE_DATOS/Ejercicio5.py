6172ede3f122ce855dccbb4b700498cc769a58ae
cesarudragon-ui <cesarudragon@gmail.com>
2026-09-24 14:06:04 +0200

    Ejercicio5

---
+++ TIPOS_DE_DATOS/Ejercicio5.py
@@ -0,0 +1,3 @@
+horastrabajadas=int(input("Ingrese las horas trabajadas: "))
+valorhora=int(input("Ingrese el valor de la hora trabajada: "))
+print(f"El total a pagar es: {horastrabajadas*valorhora}")
